970420784b17f570c6ac3476ed818608ae656949
Jorge Garcia Martinez <jorubix@proton.me>
2026-07-25 00:06:32 +0200

    load historical sp500 tickers

--- src/data/universe.py
+++ src/data/universe.py
@@ -2,9 +2,7 @@ import pandas as pd
 
 
 def get_tickers() -> list:
-    url = "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents.csv"
-
-    sp500_df = pd.read_csv(url)
+    sp500_df = pd.read_csv("./data/raw/constituents.csv")
     tickers = sp500_df["Symbol"].tolist()
     clean_tickers = [ticker.replace(".", "-") for ticker in tickers]
     return clean_tickers
